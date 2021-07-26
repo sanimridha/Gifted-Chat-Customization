9a0a0f9b6f881712be10ca9633a0ee80509658e2
sanimridha <sanimridha307@gmail.com>
2021-07-26 10:05:30 +0600

    added header

--- App.js
+++ App.js
@@ -1,21 +1,120 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState, useCallback, useEffect } from "react";
+import {
+    View,
+    SafeAreaView,
+    StatusBar,
+    Text,
+    TouchableOpacity,
+} from "react-native";
+import { GiftedChat } from "react-native-gifted-chat";
+import { Ionicons, MaterialIcons } from "@expo/vector-icons";
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+export function App() {
+    const [messages, setMessages] = useState([]);
+
+    useEffect(() => {
+        setMessages([
+            {
+                _id: 1,
+                text: "This is a quick reply. Do you love Gifted Chat? (radio) KEEP IT",
+                createdAt: new Date(),
+                quickReplies: {
+                    type: "radio", // or 'checkbox',
+                    keepIt: true,
+                    values: [
+                        {
+                            title: "😋 Yes",
+                            value: "yes",
+                        },
+                        {
+                            title: "📷 Yes, let me show you with a picture!",
+                            value: "yes_picture",
+                        },
+                        {
+                            title: "😞 Nope. What?",
+                            value: "no",
+                        },
+                    ],
+                },
+                user: {
+                    _id: 2,
+                    name: "React Native",
+                },
+            },
+            {
+                _id: 2,
+                text: "This is a quick reply. Do you love Gifted Chat? (checkbox)",
+                createdAt: new Date(),
+                quickReplies: {
+                    type: "checkbox", // or 'radio',
+                    values: [
+                        {
+                            title: "Yes",
+                            value: "yes",
+                        },
+                        {
+                            title: "Yes, let me show you with a picture!",
+                            value: "yes_picture",
+                        },
+                        {
+                            title: "Nope. What?",
+                            value: "no",
+                        },
+                    ],
+                },
+                user: {
+                    _id: 2,
+                    name: "React Native",
+                },
+            },
+        ]);
+    }, []);
+
+    const onSend = useCallback((messages = []) => {
+        setMessages(previousMessages =>
+            GiftedChat.append(previousMessages, messages)
+        );
+    }, []);
+
+    return (
+        <View style={{ flex: 1, marginTop: StatusBar.currentHeight }}>
+            <View
+                style={{
+                    // backgroundColor: "coral",
+                    height: 50,
+                    width: "100%",
+                    borderBottomColor: "#d1d1d1",
+                    borderBottomWidth: 1,
+                    flexDirection: "row",
+                    justifyContent: "space-between",
+                    alignItems: "center",
+                    paddingHorizontal: 10,
+                }}
+            >
+                <TouchableOpacity
+                    activeOpacity={0.5}
+                    style={{ flexDirection: "row", alignItems: "center" }}
+                >
+                    <Ionicons name="arrow-back" size={24} color="black" />
+                    <Text style={{ fontSize: 16 }}>Back</Text>
+                </TouchableOpacity>
+                <Text style={{ fontSize: 19 }}>Steve Jobs</Text>
+                <TouchableOpacity activeOpacity={0.5}>
+                    <Ionicons
+                        name="md-person-add-outline"
+                        size={24}
+                        color="black"
+                    />
+                </TouchableOpacity>
+            </View>
+            <GiftedChat
+                messages={messages}
+                onSend={messages => onSend(messages)}
+                user={{
+                    _id: 1,
+                }}
+            />
+        </View>
+    );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default App;
